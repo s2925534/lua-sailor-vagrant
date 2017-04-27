4fefbd1ffd6e19d1d1af97fbdcab6a00c7157ebf
Pedro Veloso <it.specialist@hotmail.com>
2017-04-27 12:48:14 +1000

    back to apache2

--- init.sh
+++ init.sh
@@ -4,18 +4,28 @@ apt-get upgrade
 
 apt-get -y install git
 apt-get -y install apache2
+#apt-get -y install nginx
 apt-get -y install lua5.2
 apt-get -y install luarocks
 apt-get -y install mysql-client
 apt-get -y install lua-sql-mysql
 
 #Enable Lua
-a2enmod lua
+a2enmod lua										#For apache2 installation only
 
-sed -i '/\<Directory \/var\/www.*/{n;n;s/None$/All/;}' /etc/apache2/apache2.conf
-service apache2 restart
+sed -i '/\<Directory \/var\/www.*/{n;n;s/None$/All/;}' /etc/apache2/apache2.conf	#For apache2 installation only
+service apache2 restart 								#For apache2 installation only
 
-# Install packages
+#service nginx restart
+
+#Install packages
+luarocks install luasql-mysql
+luarocks install luafilesystem
+luarocks install valua
+luarocks install LuaSocket
+luarocks install LuaSec
+luarocks install LuaSec OPENSSL_LIBDIR=/usr/lib/x86_64-linux-gnu
+
 luarocks install sailor
 
 #Create new sailor project
