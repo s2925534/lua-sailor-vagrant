0af58ffdfb4f2d21567c0398c108635089a849af
Pedro Veloso <it.specialist@hotmail.com>
2017-04-27 10:37:24 +1000

    Setup updates

--- init.sh
+++ init.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 apt-get update
-apt-get install -y apache2 lua5.2 luarocks
+apt-get upgrade
+
+apt-get -y install apache2
+apt-get -y install lua5.2
+apt-get -y install luarocks
+
+#Enable Lua
 a2enmod lua
+
 sed -i '/\<Directory \/var\/www.*/{n;n;s/None$/All/;}' /etc/apache2/apache2.conf
 service apache2 restart
 
 # Install packages
 luarocks install sailor
+luarocks install luasql-mysql
 
 #Create new sailor project
 sailor create 'hello-world' /vagrant/sailor
